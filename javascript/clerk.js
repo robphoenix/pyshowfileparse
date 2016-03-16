f7932ef5181822dcfd5644d72ac756edc68cbad4
Rob Phoenix <robertphoenix@gmail.com>
2016-03-16 13:24:54 +0000

    add Ramda & Immutable.js

--- javascript/clerk.js
+++ javascript/clerk.js
@@ -1,47 +1,70 @@
 "use strict";
 
-var fs = require("fs");
-var path = require("path");
+var fs = require('fs');
+var path = require('path');
+var R = require('ramda');
+var Immutable = require('immutable');
 
 var testDataDir = path.join('..', 'test_data');
-var fileName = path.join('..', 'test_data', 'elizabeth_cotton.txt');
+// var fileName = path.join('..', 'test_data', 'elizabeth_cotton.txt');
 var hostnameRegex = /(\S+)\#sh[ow\s]+ver.*/;
 var serialNumberRegex = /[Ss]ystem\s+[Ss]erial\s+[Nn]umber\s+:\s([\w]+)/g;
 var modelSoftwareRegex = /([\w-]+)\s+(\d{2}\.[\w\.)?(?]+)\s+(\w+[-|_][\w-]+\-[\w]+)/g;
 
-var fileContent = fs.readFileSync(fileName, 'utf8');
 
-// Get hostname
-var hostname = hostnameRegex.exec(fileContent)[1];
+function parseFile(fileName) {
+  fileName = path.join(testDataDir, fileName);
+  var fileContent = fs.readFileSync(fileName, 'utf8');
 
-// Get Serial Numbers
-var serialNumberArray = [];
-var serialNumberMatch;
+  // Get hostname
+  var hostname = hostnameRegex.exec(fileContent)[1];
 
-while (serialNumberMatch = serialNumberRegex.exec(fileContent)) {
-  serialNumberArray.push(serialNumberMatch[1]);
-};
+  // Get Serial Numbers
+  var serialNumberArray = [];
+  var serialNumberMatch;
 
-// Get Model, Software Version & Software Image
-var modelNumberArray = [];
-var softwareVersionArray = [];
-var softwareImageArray = [];
-var modelSoftwareMatch;
+  while (serialNumberMatch = serialNumberRegex.exec(fileContent)) {
+    serialNumberArray.push(serialNumberMatch[1]);
+  };
 
-while (modelSoftwareMatch = modelSoftwareRegex.exec(fileContent)) {
-  // console.log(modelSoftwareMatch);
-  modelNumberArray.push(modelSoftwareMatch[1]);
-  softwareVersionArray.push(modelSoftwareMatch[2]);
-  softwareImageArray.push(modelSoftwareMatch[3]);
-};
+  // Get Model, Software Version & Software Image
+  var modelNumberArray = [];
+  var softwareVersionArray = [];
+  var softwareImageArray = [];
+  var modelSoftwareMatch;
 
-console.log("Hostname: " + hostname);
-console.log("Serial Numbers: " + serialNumberArray);
-console.log("Model Numbers: " + modelNumberArray);
-console.log("Software Versions: " + softwareVersionArray);
-console.log("Software Images: " + softwareImageArray);
+  while (modelSoftwareMatch = modelSoftwareRegex.exec(fileContent)) {
+    // console.log(modelSoftwareMatch);
+    modelNumberArray.push(modelSoftwareMatch[1]);
+    softwareVersionArray.push(modelSoftwareMatch[2]);
+    softwareImageArray.push(modelSoftwareMatch[3]);
+  };
+
+  var device = Immutable.Map(
+    {
+      hostname:hostname,
+      serialNumbers:serialNumberArray,
+      modelNumbers:modelNumberArray,
+      softwareVersions:softwareVersionArray,
+      softwareImages:softwareImageArray
+    });
+
+  console.log(device);
+
+  /*
+  console.log('\n')
+  console.log("Hostname: " + hostname);
+  console.log("Serial Numbers: " + serialNumberArray);
+  console.log("Model Numbers: " + modelNumberArray);
+  console.log("Software Versions: " + softwareVersionArray);
+  console.log("Software Images: " + softwareImageArray);
+  */
+
+}
+
+
 
 // Loop through test Data directory
 fs.readdir(testDataDir, function(err, files) {
-  console.log(files);
+  R.map(parseFile, files)
 });
