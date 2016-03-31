65030ace8c4261e94ead6378acb624e106b2fc5b
Rob Phoenix <rob.phoenix@bt.com>
2016-03-31 13:52:58 +0100

    add functionality to select directory and then build inventory

--- javascript/cisco-clerk/index.js
+++ javascript/cisco-clerk/index.js
@@ -2,18 +2,19 @@ const ipcRenderer = require('electron').ipcRenderer;
 const remote = require('electron').remote;
 const dialog = remote.require('dialog');
 
-var showFilesDirButton = document.getElementById('showFilesDir');
+var showFilesDirButton = document.getElementById('showFilesDirSelect');
 
 showFilesDirButton.addEventListener('click', function (event) {
-    var showFilesDir = dialog.showOpenDialog({ properties: ['openDirectory'] });
-    console.log(showFilesDir);
-    ipcRenderer.send('build', showFilesDir);
+  var showFilesDir = dialog.showOpenDialog({ properties: ['openDirectory'] });
+  document.getElementById('showFilesDirPath').value = showFilesDir;
+    //ipcRenderer.send('build', showFilesDir);
 })
 
 var buildButton = document.getElementById('build');
 
 buildButton.addEventListener('click', function (event) {
-    ipcRenderer.send('build');
+  var showFilesDirPath = document.getElementById('showFilesDirPath').value;
+  ipcRenderer.send('build', showFilesDirPath);
 });
 
 //ipcRenderer.on('result', function (event, arg) {
